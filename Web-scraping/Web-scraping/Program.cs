1b2c3126bd65ef0ede1bd5f53a0a8f66306bf04e
Yasushi Sato <yasushi.sato1@sato-global.com>
2022-04-07 09:19:16 +0800

    First commit

---
+++ Web-scraping/Web-scraping/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using HtmlAgilityPack;
+
+//Console.WriteLine("Hello, World!");
+
+
+//var wc = new WebClient();
+var hc = new HttpClient();
+
+//string htmlStr = wc.DownloadString("https://www.yahoo.co.jp/");
+string htmlStr = await hc.GetStringAsync("https://www.yahoo.co.jp/");
+if (htmlStr == null)
+{
+    Console.WriteLine("htmlStr is null");
+    return;
+}
+
+var htmlDoc = new HtmlDocument();
+htmlDoc.LoadHtml(htmlStr);
+var node = htmlDoc.DocumentNode.SelectSingleNode("/html/head/title");
+Console.WriteLine(node.InnerText);
+
+Console.WriteLine("---------------------------------------------------------");
+
+
+var nodes = htmlDoc.DocumentNode.SelectNodes("/html/head/meta");
+foreach (var n in nodes)
+{
+    foreach(HtmlAttribute attr in n.Attributes)
+    {
+        Console.WriteLine(attr.Name + " " + attr.Value);
+    }
+}
+
+Console.WriteLine("---------------------------------------------------------");
+
+nodes = htmlDoc.DocumentNode.SelectNodes("//span");
+foreach(var n in nodes)
+{
+    Console.WriteLine(n.InnerText);
+}
